666cb87f6c3b1a679b7fb31fee7839366243f1c9
atulrpai <atulramanathpai@ymail.com>
2024-05-29 23:53:53 +0530

    function to add a check box to each message

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var intervalId;
 var menu;
 
+function addCheckboxesToConversationItems() {
+    const conversationItems = document.querySelectorAll('mws-conversation-list-item');
+
+    conversationItems.forEach(item => {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.className = 'conversation-checkbox';
+        item.appendChild(checkbox);
+    });
+}
+
 function confirmDelete() {
 	document.querySelector('.mdc-button.mat-mdc-button.primary-button.action-button.txt-button.mat-primary').click();
 }
