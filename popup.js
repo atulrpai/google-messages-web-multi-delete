2bc58d027214668ce34bb283cb9afe82f69e1fa4
atulrpai <atulramanathpai@ymail.com>
2024-05-31 00:13:03 +0530

    chrome extension needs communication from popup.js to script.js

--- popup.js
+++ popup.js
@@ -1,7 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('createCheckBoxBtn').addEventListener('click', createCheckBox);
-    document.getElementById('selectAllBtn').addEventListener('click', selectAll);
-    document.getElementById('deleteSelectedBtn').addEventListener('click', deleteSelected);
+
+	document.getElementById("createCheckBoxBtn").addEventListener("click", async () => {
+	    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+	    await chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		function: addCheckboxesToConversationItems
+	    });
+	});
+
+	document.getElementById("selectAllBtn").addEventListener("click", async () => {
+	    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+	    await chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		function: selectAllCheckboxes
+	    });
+	});
+
+	document.getElementById("deleteSelectedBtn").addEventListener("click", async () => {
+	    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+	    await chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		function: deleteSelectedConversationItems
+	    });
+	});
+
 });
 
 function createCheckBox() {
@@ -15,4 +37,3 @@ function selectAll() {
 function deleteSelected() {
     // Write your logic to delete selected checkboxes here
 }
-
